802ab336fef2e810654098ec1ba002c83e7adf0d
vasukotadiya7 <vasukotadiya878op@gmail.com>
2023-09-10 23:30:47 +0530

    first phase

---
+++ backend/mongo/resource.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://Vasu:odYQZn2o9ZsYL2rd@cluster0.ppjsbe7.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+var db = mongoose.connection;
+try {
+  db.on("error", console.error.bind(console, "Connecton error"));
+  db.once("open", function () {
+    console.log("mongoDB connected");
+  });
+} catch (err) {
+  console.log(err);
+}
+
+const ResourceRepositorySchema = new mongoose.Schema({
+  r_id: { type: String, require: true },
+  subject: { type: String, required: true },
+  r_name: { type: String, required: true },
+  r_data: { type: String },
+});
+exports.AdminDashboard = new mongoose.model(
+  "ResourceRepository",
+  ResourceRepositorySchema
+);
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,63 +5,128 @@ const Router = require("router");
 const { mongo } = require("mongoose");
 const router = Router();
 
-
 let user;
 let admin;
 // user login request
 router.post("/User", async (req, res) => {
-    let success = false;
-    const { email, password } = req.body;
-    user = await User.findOne({ email: email });
-    console.log(user)
-    console.log(email)
-    console.log(password)
+  let success = false;
+  const { email, password } = req.body;
+  user = await User.findOne({ email: email });
+  console.log(user);
+  console.log(email);
+  console.log(password);
 
-    if (!email || !password) {
+  if (!email || !password) {
+    success = false;
+    res.send({ message: "Please enter valid data" });
+  } else {
+    if (user) {
+      success = true;
+      if (user.password === password) {
+        res.send({
+          message: "Login Successfull",
+          user: user,
+          success: success,
+        });
+      } else {
         success = false;
-        res.send({ message: "Please enter valid data" });
+        res.send({
+          message: "Password didn't match",
+        });
+        // res.sendStatus(401);
+      }
     } else {
-        if (user) {
-            success = true;
-            if (user.password === password) {
-                res.send({ message: "Login Successfull", user: user, success: success });
-            } else {
-                success = false;
-                res.send({
-                    message: "Password didn't match",
-                });
-                // res.sendStatus(401);
-            }
-        } else {
-            success = false;
-            res.send({ message: "User not Register." });
-        }
+      success = false;
+      res.send({ message: "User not Register." });
     }
+  }
 });
-
 
 // User  request
 router.post("/UserRegister", async (req, res) => {
-    try {
-        const { name, email, password, department, subject } = req.body;
+  try {
+    const { name, email, password, department, subject } = req.body;
 
-        const UserDetail = new User({
-            name, email, password, department, subject
-        });
-        const doc = await UserDetail.save();
-        console.log(doc);
-        return res.status(200).json({
-            success: true,
-            message: "entery created",
-            doc: doc
-        })
-    } catch (err) {
-        return res.status(500).json({
-            success: false,
-            message: err.message,
-        })
-    }
+    const UserDetail = new User({
+      name,
+      email,
+      password,
+      department,
+      subject,
+    });
+    const doc = await UserDetail.save();
+    console.log(doc);
+    return res.status(200).json({
+      success: true,
+      message: "entery created",
+      doc: doc,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
 });
 
+// User Delete
+router.post("/UserDelete", async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    const UserDetail = new User({
+      email,
+    });
+    const doc = await UserDetail.remove({ email: email });
+    // console.log(doc);
+    return res.status(200).json({
+      success: true,
+      message: "entery deleted",
+      doc: doc,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+// User update
+router.post("/UserUpdate", async (req, res) => {
+  try {
+    const { name, password, department, subject, email } = req.body;
+
+    const UserDetail = new User({
+      name,
+      password,
+      department,
+      subject,
+      email,
+    });
+    const doc = await UserDetail.findOneAndUpdate(
+      { email: email },
+      {
+        $set: {
+          name: name,
+          password: password,
+          department: department,
+          subject: subject,
+        },
+      }
+    );
+    // console.log(doc);
+    return res.status(200).json({
+      success: true,
+      message: "entery deleted",
+      doc: doc,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
 
 exports.router = router;
