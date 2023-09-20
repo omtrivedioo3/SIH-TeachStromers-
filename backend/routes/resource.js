32208e9c68d933ff7cd1f53dc5ce222833a43202
vasukotadiya7 <vasukotadiya878op@gmail.com>
2023-09-20 15:33:15 +0530

    resource repo

--- backend/routes/resource.js
+++ backend/routes/resource.js
@@ -6,33 +6,39 @@ const Router = require("router");
 const { mongo } = require("mongoose");
 const router = Router();
 
-
 router.post("/resourceRepository", async (req, res) => {
-    try {
-        const { r_id,
-            subject,
-            r_name,
-            r_data, } = req.body;
+  try {
+    const { r_id, subject, r_name, r_data } = req.body;
 
-        const newResource = new resource({
-            r_id,
-            subject,
-            r_name,
-            r_data,
-        });
-        const doc = await newResource.save();
-        console.log(doc);
-        return res.status(200).json({
-            success: true,
-            message: "entery created",
-            doc: doc,
-        });
-    } catch (err) {
-        return res.status(500).json({
-            success: false,
-            message: err.message+"not",
-        });
-    }
+    const newResource = new resource({
+      r_id,
+      subject,
+      r_name,
+      r_data,
+    });
+    const doc = await newResource.save();
+    console.log(doc);
+    return res.status(200).json({
+      success: true,
+      message: "entery created",
+      doc: doc,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: err.message + "not",
+    });
+  }
+});
+
+//to get all resorce repository
+router.get("/getAllResourceRepository", async (req, res) => {
+  try {
+    const allresource = await resource.find({});
+    res.send({ data: allresource });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 exports.router = router;
--- loginrepo/src/components/dashboard/ShowResource.js
+++ loginrepo/src/components/dashboard/ShowResource.js
@@ -1,24 +1,64 @@
-import React, { } from 'react'
-import { useNavigate, Navigate } from 'react-router-dom';
-
+import React, { useEffect, useState } from "react";
+import { useNavigate, Navigate } from "react-router-dom";
 
 function ShowResource() {
+  const [resoursces, setResources] = useState("");
   const Navigate = useNavigate();
+
+  const getResources = async (e) => {
+    // e.preventDefault();
+    const res = await fetch(
+      "http://localhost:5000/resource/getAllResourceRepository",
+      {
+        method: "GET",
+        // body: JSON.stringify(user),
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+    const resources = await res.json();
+    setResources(resources.data);
+    console.log(resources, "resource data");
+  };
+
+  useEffect(() => {
+    getResources();
+  }, []);
 
   const backbutton = () => {
     Navigate("/UserDashboard");
-  }
+  };
   return (
     <div>
-      THIS IS RESOURCE-REPOSITORY
+      <table style={{ width: 500 }}>
+        <tr>
+          <th>ID</th>
+          <th>Subject</th>
+          <th>Name</th>
+          <th>Link</th>
+        </tr>
+        {resoursces &&
+          resoursces.map((i) => {
+            return (
+              <tr>
+                <td>{i.r_id}</td>
+                <td>{i.subject}</td>
+                <td>{i.r_name}</td>
+                <td>
+                  <a href={i.r_data} target="_blank">
+                    View
+                  </a>
+                </td>
+              </tr>
+            );
+          })}
+      </table>
       <div className="d-grid">
         <button onClick={backbutton} type="back" className="btn btn-primary">
           Back
         </button>
       </div>
     </div>
-  )
-
+  );
 }
 
-export default ShowResource
+export default ShowResource;
